288db35e2e33abb0e21f409924154275632843b4
Philip Adelabu <adebisiphilip@gmail.com>
2025-11-11 17:19:13 +0100

    worked on the menu nav animation for mobile devices

--- chamber/scripts/gridlist.js
+++ chamber/scripts/gridlist.js
@@ -1,18 +1,18 @@
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
+const display = document.querySelector("#display");
 
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
 gridbutton.addEventListener("click", () => {
 	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
+	display.classList.add("business-cards");
+	display.classList.remove("business-cards-list");
 });
 
 listbutton.addEventListener("click", showList); // example using defined function
 
 function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
+	display.classList.add("business-cards-list");
+	display.classList.remove("business-cards");
 }
