0d4537a63b609cc95dc9fde2174b3829ec1ef6df
Philip Adelabu <adebisiphilip@gmail.com>
2025-11-16 09:46:33 +0100

    modified the directory page for reflecting the new adjustment made from the index page

--- chamber/scripts/getweather.js
+++ chamber/scripts/getweather.js
@@ -15,11 +15,12 @@
      if(response.ok) {
      const weatherData = await response.json();
      if(weatherData){
-     console.log(weatherData);
      document.getElementById("temperature").innerHTML = `Temparature: <strong>${weatherData.main.temp.toFixed(0)} &deg;F</strong>`;
      document.getElementById("description").innerHTML = `Description: <strong>${weatherData.weather[0].description}</strong>`;
      document.getElementById("humidity").innerHTML = `Humidity: <strong>${weatherData.main.humidity}%</strong>`;
- 
+      iconurl = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+     document.getElementById("weathericon").setAttribute('src', iconurl);  
+    
     }else{
         throw Error(await response.text());
     }
@@ -43,16 +44,12 @@ function getDayName(offset) {
 }
 
 
-
- /* api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9a96722a702555a9a431e0dc0783d7ab */
- /* api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={API key}  */
-
 const lat = "6.52";
 const lon = "3.37";
 const cnt = 3;
-const forecasetKey = "2cf87d0ad00f6417c56743d6382dae18";
-const forecasturl = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=${cnt}&appid=${apikey}`;
 
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=${cnt}&appid=${apikey}`;
+ 
 async function getForecast() {  
     
     const day1Name = getDayName(0);
@@ -60,22 +57,24 @@ async function getForecast() {
     const day3Name = getDayName(2);
 
     try{
-     const response = await fetch(forecasturl);
+     const response = await fetch(forecastURL);
      if(response.ok) {
      const forecastData = await response.json();
      if(forecastData){
      console.log(forecastData);
+     
      // Day 1
-     document.getElementById("day1-temp").innerHTML = `${day1Name}: <strong> ${forecastData.list[0].temp.max.toFixed(0)} &deg;F</strong><br>
-     <strong>Low: ${forecastData.list[0].temp.min.toFixed(0)} &deg;F</strong>`;
+     document.getElementById("day1-temp").innerHTML = `<strong>${day1Name}</strong>: <br>Max: ${forecastData.list[0].main.temp_max} &deg;F<br>
+     Low: ${forecastData.list[0].main.temp_min} &deg;F`;
  
      // Day 2
-     document.getElementById("day2-temp").innerHTML = ` ${day2Name}: <strong> ${forecastData.list[1].temp.max.toFixed(0)} &deg;F</strong><br>
-     <strong>Low: ${forecastData.list[1].temp.min.toFixed(0)} &deg;F</strong>`;
+     document.getElementById("day2-temp").innerHTML = `<strong>${day2Name} </strong>: <br>Max: ${forecastData.list[1].main.temp_max} &deg;F<br>
+     Low: ${forecastData.list[1].main.temp_min} &deg;F`;
  
      // Day 3                                       
-        document.getElementById("day3-temp").innerHTML = `${day3Name}:  <strong> ${forecastData.list[2].temp.max.toFixed(0)} &deg;F</strong><br>         
-        <strong>Low: ${forecastData.list[2].temp.min.toFixed(0)} &deg;F</strong>`;      
+        document.getElementById("day3-temp").innerHTML = `<strong>${day3Name}</strong>:  <br>Max: ${forecastData.list[2].main.temp_max} &deg;F<br>         
+        Low: ${forecastData.list[2].main.temp_min} &deg;F`;      
+      
     }else{         
         throw Error(await response.text());      
     }     
